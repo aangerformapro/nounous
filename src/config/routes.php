4293f425ba6e1810c1b88642fbe08ba9e102fa7d
Aymeric Anger <a.anger@codeur.online>
2023-08-16 12:03:52 +0200

    slim initialization

--- src/config/routes.php
+++ src/config/routes.php
@@ -1,7 +1,13 @@
 <?php
 
 use Slim\App;
+use Slim\Views\PhpRenderer;
 
 return function (App $app)
 {
+    $app->get('/hello/{name}', function ($request, $response, $args)
+    {
+        $renderer = $this->get(PhpRenderer::class);
+        return $renderer->render($response, 'index.php', $args);
+    })->setName('profile');
 };
