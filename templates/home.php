6f230d0d9af6ba958e2828cf3856448de2aebe0a
ayyobi <ayyobinargis@gmail.com>
2023-08-18 14:02:43 +0200

    git pull

--- templates/home.php
+++ templates/home.php
@@ -8,6 +8,17 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
+
+
+
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="/assets/pictures/logo/logox64.webp" alt="Logo" width="64" height="64" class="d-inline-block align-text-top">
+      DailySitter
+    </a>
+  </div>
+</nav>
     
 </body>
 </html>
