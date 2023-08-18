9beda83b2b50d1dec782053a0e366792076259b6
ayyobi <ayyobinargis@gmail.com>
2023-08-18 11:49:32 +0200

    git pull

--- templates/login.php
+++ templates/login.php
@@ -2,7 +2,7 @@
 
 <div class="page d-flex justify-content-center align-items-center">
 <form method="post" action="/login" class="form-login">
-<h1 class="p-3">Connexion</h1>
+<h1 class="p-3 text-white">Connexion</h1>
     <div class="form-floating mb-3">
     <input type="email" class="form-control" name="email" placeholder="Email" placeholder="Votre adresse Email" required>
     <label for="floatingInput">Votre adresse Email</label>
@@ -13,16 +13,16 @@
     </div>
   <div class="mb-3 form-check">
     <input type="checkbox" class="form-check-input" name="remember">
-    <label class="form-check-label" for="remember">Se souvenir de moi</label>
+    <label class="form-check-label text-white" for="remember">Se souvenir de moi</label>
   </div>
 
   <div class="text-center">
-     <button type="submit" class="btn btn-outline-danger">Se connecter</button>
+     <button type="submit" class="btn btn-outline-danger ">Se connecter</button>
   </div>
 
   <div class="">
-    <a href="/register" title="Créer un compte">Créer un compte</a><br>
-    <a href="/login/recover" title="Mot de passe oublié?">Mot de passe oublié?</a>
+    <a href="/register" title="Créer un compte" class="text-white">Créer un compte</a><br>
+    <a href="/login/recover" title="Mot de passe oublié?" class="text-white">Mot de passe oublié?</a>
 
   </div>
  
