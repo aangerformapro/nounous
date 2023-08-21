ed540e7b28c2a9e820ee0ea4a6f90fcbb7d90799
ayyobi <ayyobinargis@gmail.com>
2023-08-21 15:38:47 +0200

    commit

--- templates/home.php
+++ templates/home.php
@@ -2,6 +2,19 @@
   <?php include 'layout/nav.php'; ?>
   <div class="page">
 
+      <div class="intro">
+        <h2>Trouvez la meilleure garde d'enfant !</h2>
+        <p>Garde périscolaire, baby-sitting ponctuel, assistante maternelle : nous sommes
+           le meilleur site français de mise en relation parents / baby-sitters !</p>
+        <div class="text-center">
+          <button type="submit" class="btn text-white" style="background:#ea523d;">Se connecter</button>
+        </div>
+
+      </div>
+      <div class="img">
+        <img src="" alt="home-img">
+      </div>
+
   </div>
 </div>
 
