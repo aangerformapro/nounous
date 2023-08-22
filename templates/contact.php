c076908785732f5b66903f9e7ea9f083e462aa3c
Aymeric Anger <a.anger@codeur.online>
2023-08-22 15:58:45 +0200

    push

--- templates/contact.php
+++ templates/contact.php
@@ -1,28 +1,29 @@
-<div class="app page-home">
+<div class="app page-contact">
   <?php include 'layout/nav.php'; ?>
 
   <div class="page container d-flex flex-nowrap flex-column flex-lg-row mx-auto">
 
-      <div class="intro d-flex flex-column justify-content-center p-5 col-lg-6">
-        <h2>Contactez-nous</h2>
-        <p class="d-none d-lg-flex">Vous avez une demande d'informations ou une question?</p>
-        <div class="">
+      <div class="intro d-flex flex-column justify-content-center mt-5 mt-lg-0 p-5 col-lg-6">
+        <h2 class="mb-3">Contactez-nous</h2>
+        <p class="d-flex">Vous avez une demande d'informations ou une question?</p>
+        <div class="d-flex flex-column">
           <a href="mailto:dailysitter@gmail.com">
-            <svg fill="currentColor" class="ng-svg-icon" width="22" height="22">
+            <svg fill="currentColor" class="ng-svg-icon me-3" width="25" height="25">
               <use xlink:href="#ng-mail"></use>
             </svg>
             dailysitter@gmail.com
           </a>
-          <div>
-          <svg fill="currentColor" class="ng-svg-icon" width="22" height="22">
+         
+          <a href="tel:0759468231">
+          <svg fill="currentColor" class="ng-svg-icon me-3" width="25" height="25">
               <use xlink:href="#ng-phone"></use>
             </svg>
             0759468231
-          </div>
+          </a>
         </div>
       </div>
       <div class="home-img d-none d-lg-flex flex-column justify-content-center align-items-center">
-        <img src="/assets/pictures/home-resized.webp" alt="home-img">
+        <img src="/assets/pictures/contact.webp" alt="home-img">
       </div>
 
   </div>
